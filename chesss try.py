8b46626d6689ce157add2616453e879b8669900a
unknown <261031575@formanite.fcollege.edu.pk>
2024-03-25 18:48:52 +0500

    Updatation

--- chesss try.py
+++ chesss try.py
@@ -1,15 +1,17 @@
+#Updatation by learning More...!
+
 from turtle import *
 from random import *
 shelly = Turtle()
 shelly.speed(0)
-#1
-def row(x = -500,y = 250):
+
+#even Row
+def evenRow(x,y):
     shelly.penup()
     shelly.goto(x,y)
     shelly.pendown()
     num = 100
-    num2 = 100
-    if x ==500:
+    if x ==500: #Base Condition
         return 0    
     else:
         if ((-(x))//100)%2==0:
@@ -23,16 +25,16 @@ def row(x = -500,y = 250):
             for i in range(4):
                 shelly.forward(100)
                 shelly.left(90)
-    row(x+num)
     
-row()
-#2
-def row(x = -500,y = 150):
+    #Recursive Call
+    evenRow(x+num,y)
+
+#odd Row 
+def oddRow(x,y):
     shelly.penup()
     shelly.goto(x,y)
     shelly.pendown()
     num = 100
-    num2 = 100
     if x ==500:
         return 0
     else:
@@ -47,131 +49,15 @@ def row(x = -500,y = 150):
                 shelly.forward(100)
                 shelly.left(90)
             shelly.end_fill()
-            
-    row(x+num)
+    
+    #Recursive Call        
+    oddRow(x+num,y)
 
-row()
-#3
-def row(x = -500,y = 50):
-    shelly.penup()
-    shelly.goto(x,y)
-    shelly.pendown()
-    num = 100
-    num2 = 100
-    if x ==500:
-        return 0
-    else:
-        if ((-(x))//100)%2==0:
-            shelly.color("black")
-            shelly.begin_fill()
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-            shelly.end_fill()
-        else:
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-    row(x+num)
-
-row()
-#4
-def row(x = -500,y = -50):
-    shelly.penup()
-    shelly.goto(x,y)
-    shelly.pendown()
-    num = 100
-    num2 = 100
-    if x ==500:
-        return 0
-    else:
-        if (-(x//100))%2==0:
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-        else:
-            shelly.color("black")
-            shelly.begin_fill()
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-            shelly.end_fill()
-            
-    row(x+num)
-
-row()
-
-#5
-def row(x = -500,y = -150):
-    shelly.penup()
-    shelly.goto(x,y)
-    shelly.pendown()
-    num = 100
-    num2 = 100
-    if x ==500:
-        return 0
-    else:
-        if ((-(x))//100)%2==0:
-            shelly.color("black")
-            shelly.begin_fill()
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-            shelly.end_fill()
-        else:
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-    row(x+num)
-
-row()
-#6
-def row(x = -500,y = -250):
-    shelly.penup()
-    shelly.goto(x,y)
-    shelly.pendown()
-    num = 100
-    num2 = 100
-    if x ==500:
-        return 0
-    else:
-        if (-(x//100))%2==0:
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-        else:
-            shelly.color("black")
-            shelly.begin_fill()
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-            shelly.end_fill()
-            
-    row(x+num)
-
-row()
-
-#7
-def row(x = -500,y = -350):
-    shelly.penup()
-    shelly.goto(x,y)
-    shelly.pendown()
-    num = 100
-    num2 = 100
-    if x ==500:
-        return 0
-    else:
-        if ((-(x))//100)%2==0:
-            shelly.color("black")
-            shelly.begin_fill()
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-            shelly.end_fill()
-        else:
-            for i in range(4):
-                shelly.forward(100)
-                shelly.left(90)
-    row(x+num)
-
-row()
+#Calling Functions
+evenRow(-500,150)
+oddRow(-500,250)
+evenRow(-500,-50)
+oddRow(-500,50)
+evenRow(-500,-250)
+oddRow(-500,-150)
+oddRow(-500,-250)
